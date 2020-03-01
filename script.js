bd229e624ba2ab28931fe54c9b0f0659e4ad349d
Cassy Pearson <seepearson@gmail.com>
2020-02-29 18:22:00 -0700

    Able to choose lower or uppercase characters and has password length of 10

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
+  var length = 10;
+  var allowedCharacters = ""
+  var password = ""
+
+
+  var lower = confirm("Would you like to use lowercase letters");
+  if (lower === true) {
+    allowedCharacters += "abcdefghijklmnopqrstuvwxyz"
+
+  }
+  var upper = confirm("Would you like to use upperrcase letters");
+  if (upper === true) {
+    allowedCharacters += "abcdefghijklmnopqrstuvwxyz".toUpperCase()
+
+  }
+  for (var i = 0; i < length; i++) {
+    password += allowedCharacters[0]
+  }
+  return password
+
+
+
+
+}
+
+
+// //promts for password length
+// function buttonclicked(){
+// var pwlength = prompt("how long would you like your password? choose between 8-128 characters");
+// if(pwlength < 8 || pwlength > 128) {
+//   alert("please choose between 8-128");
+//   return buttonclicked();
+// }
